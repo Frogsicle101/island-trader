c4a708a756a66f4987bc4471baf7d96592cc45ba
Andrew Hall <amh284@uclive.ac.nz>
2021-04-21 11:48:46 +1200

    Added docs and a getter to Weather

--- src/Weather.java
+++ src/Weather.java
@@ -1,4 +1,12 @@
 import java.util.Random;
+
+/**
+ * Random event representing poor weather.
+ * When this event is drawn, the player's ship takes a random
+ * amount of damage, which must be repaired at the next island.
+ * @author Andrew Hall
+ *
+ */
 public class Weather extends RandomEvent {
 	
 	
@@ -8,5 +16,9 @@ public class Weather extends RandomEvent {
 	public Weather(float probability) {
 		super(probability);
 		damage = new Random().nextFloat() * DAMAGE_SCALE;
+	}
+	
+	public float getDamage() {
+		return damage;
 	}
 }
