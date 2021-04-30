a8e14aa168d7d1a4fb7d911f849212da49425a9e
Andrew Hall <amh284@uclive.ac.nz>
2021-04-30 13:27:00 +1200

    Modified how Islands and Routes work

--- src/Route.java
+++ src/Route.java
@@ -1,5 +1,6 @@
+
 /**
- * This class links together various island classes. 
+ * This class links two islands together. 
  * 
  * Routes have a distance, and an array of random events.
  * The distance affects how long it will take a ship to get to a destination.
@@ -10,17 +11,37 @@
  */
 public class Route {
 	
-	private int distance;
-	private RandomEvent[] randomEvents;
+	private Island[] islands;			// 2-len of the islands it connects
+	private int distance;				// Higher distance routes take longer to finish
+	private RandomEvent[] randomEvents;	// Random events have a chance of triggering on every day spent sailing
 	
 	/**
 	 * @param distance How long the route is
 	 * @param randomEvents The list of random events
 	 */
-	public Route(int distance, RandomEvent[] randomEvents) {
+	public Route(Island island1, Island island2, int distance, RandomEvent[] randomEvents) {
+		this.islands = new Island[]{island1, island2};
 		this.distance = distance;
 		this.randomEvents = randomEvents;
 	}
+	
+	/**
+	 * Every route connects 2 islands, this gets those islands
+	 * @return 2-length array of the islands this route connects
+	 */
+	public Island[] getIslands() {
+		return islands;
+	}
+	
+	/**
+	 * Since routes are two-way connection, checks if this connects the given island
+	 * @param island Island we're checking
+	 * @return true if this route connects to that island 
+	 */
+	public boolean connectsIsland(Island island) {
+		return islands[0] == island || islands[1] == island;
+	}
+	
 	
 	/**
 	 * @return the distance
