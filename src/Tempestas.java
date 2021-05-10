c84a19e3c37695c4c65bb1e0ead07fd25e45ab65
fma107 <fma107@uclive.ac.nz>
2021-05-10 21:06:06 +1200

    Added Tempestas, an Island surrounded by storms

---
+++ src/Tempestas.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class Tempestas extends Island {
+
+	public Tempestas() {
+		this.name = "Tempestas";
+		this.description = "Surrounded by storms, not many traders brave these waters. The rain is good for growing crops though";
+		// ----- Generate the store prices -----
+		HashMap<TradeTypes, Float> prices = Store.getDefaultPrices();
+		// Food is very cheap
+		prices.put(TradeTypes.FOOD, 0.2f);
+		
+		// Everything else is slightly more expensive than normal
+		prices.put(TradeTypes.TOOL, 1.2f);
+		prices.put(TradeTypes.ARTIFACT, 1.2f);
+		prices.put(TradeTypes.LUXURY, 1.4f);
+		
+		
+		Store store = new Store(this.name, prices);
+		this.store = store;
+		
+	}
+
+}
