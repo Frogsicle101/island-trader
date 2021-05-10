44e9ee71622cebdd56777052e987b4e7d0d61349
fma107 <fma107@uclive.ac.nz>
2021-05-10 20:48:27 +1200

    Added Fodina, a mining island

---
+++ src/Fodina.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class Fodina extends Island {
+
+	public Fodina() {
+		this.name = "Fodina";
+		this.description = "The main town on Fodina is almost entirely underground. "
+				+ "Its expansive natural cave network gives the miners here easy access to its mineral riches.";
+		// ----- Generate the store prices -----
+		HashMap<TradeTypes, Float> prices = Store.getDefaultPrices();
+		// Tools and Food are in high demand for the workers
+		prices.put(TradeTypes.TOOL, 1.5f);
+		prices.put(TradeTypes.FOOD, 1.4f);
+		// Materials and Gems (Luxury goods) are cheap here
+		prices.put(TradeTypes.LUXURY, 0.8f);
+		prices.put(TradeTypes.MATERIAL, 0.6f);
+		Store store = new Store(this.name, prices);
+		this.store = store;
+		
+	}
+
+}
