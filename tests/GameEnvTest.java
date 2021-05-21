b660355168aa0ff911113f931ea929ed7c0e4b75
Andrew Hall <amh284@uclive.ac.nz>
2021-05-21 12:52:53 +1200

    Moved TestShip into its own class

--- tests/GameEnvTest.java
+++ tests/GameEnvTest.java
@@ -310,19 +310,3 @@ class GameEnvTest {
 	}
 }
 
-
-class TestShip extends Ship {
-
-	public TestShip() {
-		super(1f, 		// speed
-			  "Test",	// shipType,
-			  "Words",	// description,
-			  10,		// crew,
-			  10,		// cargoCapacity,
-			  5f,		// repairCost,
-			  0			// startingMoney
-		);
-		// TODO Auto-generated constructor stub
-	}
-	
-}
---
+++ tests/TestShip.java
@@ -0,0 +1,16 @@
+
+class TestShip extends Ship {
+
+	public TestShip() {
+		super(1f, 		// speed
+			  "Test",	// shipType,
+			  "Words",	// description,
+			  10,		// crew,
+			  10,		// cargoCapacity,
+			  5f,		// repairCost,
+			  0			// startingMoney
+		);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
