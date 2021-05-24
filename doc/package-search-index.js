9263d73fdcadc7c818ac1b16a85f5f914fb4f1ff
Frederik Markwell <fma107@uclive.ac.nz>
2021-05-25 11:17:38 +1200

    Updated javadoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"cli"},{"l":"events"},{"l":"exceptions"},{"l":"gui"},{"l":"islands"},{"l":"items"},{"l":"main"},{"l":"ships"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"cli"},{"l":"events"},{"l":"exceptions"},{"l":"gui"},{"l":"islands"},{"l":"items"},{"l":"main"},{"l":"ships"}]
